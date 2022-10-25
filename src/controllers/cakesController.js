8850178e6b8bc6c67d49e649124b1696f750c05d
Robinson_Xavier <robinson.webzen@gmail.com>
2022-10-25 01:44:26 -0300

    added createCake function and cakeSchema

---
+++ src/controllers/cakesController.js
@@ -0,0 +1,32 @@
+import { connection } from '../database/db.js';
+
+import {cakeSchema} from '../schemas/cakeSchema.js';
+
+
+async function createCake (req, res) {
+    const {name, price, image, description} = req.body;
+
+    const validation = cakeSchema.validate(req.body, {abortEarly: false});
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(400).send(errors);
+    }
+
+    try {
+
+        const checkName = connection.query(`SELECT * FROM cakes WHERE name = $1;`, [name]);
+
+        if(checkName.rows[0]) {
+            return res.sendStatus(409);
+        }
+
+        connection.query(`INSERT INTO cakes (name, price, image, description) VALUES ($1, $2, $3, $4);`
+        ,[name, price, image, description]);
+
+        
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+};
---
+++ src/schemas/cakeSchema.js
@@ -0,0 +1,11 @@
+import joi from 'joi';
+
+
+const cakeSchema = joi.object({
+    name: joi.string().min(2).required(),
+    price: joi.number().min(1).precision(5).required(),
+    image: joi.string().uri().required(),
+    description: joi.string().allow('').required()
+})
+
+export {cakeSchema};
